e676598fb5229fc7adf687a7a9b10fa1049c0e52
shamima-akter20 <shamimamirpur2@gmail.com>
2025-06-13 22:51:39 +0600

    Initial commit

---
+++ src/componants/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">Page Not Found</h2>
+      <h1>Error status: 404 </h1>
+    </div>
+  )
+}
+4
+
+export default ErrorPage
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './componants/Layouts/Root'
+import ErrorPage from './componants/ErrorPage/ErrorPage'
+import Home from './componants/Home/Home'
+import Dashboard from './componants/Dashboard/Dashboard'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
