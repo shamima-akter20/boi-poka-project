3744a1f4cb4772b8b7ca756c1d5f47914a5aa193
shamima-akter20 <shamimamirpur2@gmail.com>
2025-06-14 23:58:02 +0600

    adding json data and making card

---
+++ src/componants/Books/Book.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Book = ({ book }) => {
+  const {
+    image,
+    category,
+    bookName,
+    bookAuthor,
+    bookType,
+    rating,
+    totalpage,
+    review,
+    publisher,
+    publishingdate,
+  } = book
+
+  return (
+    <div className="">
+      <div className="card bg-base-100 w-56 shadow-sm">
+        <figure className="bg-gray-200">
+          <img src={image} alt={bookName} className="h-[166px]" />
+        </figure>
+        <div className="p-2">
+          <h2 className=" font-bold flex gap-2">
+            {bookName}
+            <div className="badge bg-blue-100 text-xs">{bookType}</div>
+          </h2>
+          <div>
+            <p className="text-sm font-libra py-2">By : {bookAuthor}</p>
+            <p className="text-xs font-libra py-2">Publisher : {publisher}</p>
+            <p className="text-xs font-libra pb-2">Date : {publishingdate}</p>
+          </div>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Book
--- src/componants/Books/Books.jsx
+++ src/componants/Books/Books.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Book from './Book'
 
 const Books = () => {
-  return <div></div>
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetch('../../../public/booksData.json')
+      .then((res) => res.json())
+      .then((data) => setBooks(data))
+  }, [])
+
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-center font-libra my-10">Books</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {books.map((book) => (
+          <Book book={book} key={book.id}></Book>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Books
